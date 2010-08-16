a07cce1aead14e003e7421494af4a13156b7ed26
Marian Theisen <marian@cice-online.net>
2010-08-16 15:02:35 +0200

    rails 3 project generated, added basic gem dependencies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CRuMble::Application.config.secret_token = 'eb225abfa047c703e3ab1fc484f7c17478ab84fb172999150442a6071ecfb11d3dfc0852aa8260056ce8707e390c8875942a4215044004c51e32c07ed83fb04a'
