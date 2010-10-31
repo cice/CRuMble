c6336e15ea43b829a5f7ed3032c309457ecb2fb5
Marian Theisen <marian@cice-online.net>
2010-10-31 19:35:50 +0100

    ehhhh

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,9 +13,9 @@ class NotesController < ApplicationController
     
     if @note.save
       
-      note_attribs = @note.attributes.except(:noted_id, :collaborator_id, :collaborator_type).merge({:noted => @note.noted, :collaborator => @note.collaborator, :collaborator_link => collaborator_path(@note.collaborator)})
-
-      render :json => note_attribs
+      note_attribs = @note.attributes.except("noted_id", "collaborator_id", "collaborator_type").merge({:noted => @note.noted.attributes, :collaborator => @note.collaborator.attributes, :collaborator_link => collaborator_path(@note.collaborator)})
+            
+      render :json => {:note => note_attribs}
       Event.throw ({"NoteTaker" => current_user.collaborator, "Noted" => @note.noted},"NoteTaken")
     else
       render :action => 'new'
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,8 @@
 $(function(){
+  $.getJSON('http://localhost:3000/notes/4.json', function(data){
+    alert(data.note);
+  });
+  
   $('#noteForm').hide();
   $('#modal_opacity').hide().removeClass('.hidden');
   
@@ -33,31 +37,29 @@ $(function(){
   
 
   jQuery("form.new_note").bind('ajax:success', function(data, status, xhr){
-
+  
     $("#noteForm").stop().animate({'top' : '-400px'}, 200, function () {
-
+    
       $("#noteForm").hide();
-
+    
       // $("#stickyNotesBoard").html("schipps");
-
+    
       // alert(status);
-
+    
        //$("#noteJsonTempl").tmpl(status).prependTo("#stickyNotesBoard");
-
+    
     });
-
+    
     $("#modal_opacity").fadeOut(200);
     this.reset();
     return false;
   });
-
+  
   jQuery('#cancel_note').click(function () {
-    $("#noteForm").stop().animate({'top' : '-400px'}, 200, function () {$("#noteForm").hide()});
+    $("#noteForm").stop().animate({'top' : '-400px'}, 200, function() {
+      $("#noteForm").hide();
+    });
     $("#modal_opacity").fadeOut(200);
     return false;
   });
-
-
-
-
 });
