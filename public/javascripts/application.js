5a424f98204c1773780fe4f40b17c24efe15fa8e
Michael Eickenberg <michael@cice-online.net>
2010-10-31 09:55:55 -0700

    put _note_form javascript into application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,33 +1,63 @@
 $(function(){
-	$('#noteForm').hide();
-	$('#modal_opacity').hide().removeClass('.hidden');
-	
-	
-	$('.noteLink a').click(
-	
-		function(){
-			var e = $(this);
-			var typ = e.attr('data-noted_type');
-			var id = e.attr('data-noted_id');
-			var nam =e.attr('data-noted_name');
-			alert('type: '+typ+'\nid= '+id);
-			
-			$('#noteForm #note_noted_id').attr('value',id);
-			$('#noteForm #note_noted_type').attr('value',typ);
-			
-			$('#noteForm .field label').html('Take a note for '+nam);
-			
-			$('#noteForm')	.show()
-							.animate({'top' : 0},300);
-			$('#modal_opacity').fadeIn(500);
-		
-		
-			return false;
-		}
-	
-	
-	
-	
-	);
-	
+  $('#noteForm').hide();
+  $('#modal_opacity').hide().removeClass('.hidden');
+  
+  
+  $('.noteLink a').click(
+  
+    function(){
+      var e = $(this);
+      var typ = e.attr('data-noted_type');
+      var id = e.attr('data-noted_id');
+      var nam =e.attr('data-noted_name');
+      alert('type: '+typ+'\nid= '+id);
+      
+      $('#noteForm #note_noted_id').attr('value',id);
+      $('#noteForm #note_noted_type').attr('value',typ);
+      
+      $('#noteForm .field label').html('Take a note for '+nam);
+      
+      $('#noteForm')  .show()
+              .animate({'top' : 0},300);
+      $('#modal_opacity').fadeIn(500);
+    
+    
+      return false;
+    }
+  
+  
+  
+  
+  );
+  
+  
+
+  jQuery("form.new_note").bind('ajax:success', function(data, status, xhr){
+
+    $("#noteForm").stop().animate({'top' : '-400px'}, 200, function () {
+
+      $("#noteForm").hide();
+
+      // $("#stickyNotesBoard").html("schipps");
+
+      // alert(status);
+
+       //$("#noteJsonTempl").tmpl(status).prependTo("#stickyNotesBoard");
+
+    });
+
+    $("#modal_opacity").fadeOut(200);
+    this.reset();
+    return false;
+  });
+
+  jQuery('#cancel_note').click(function () {
+    $("#noteForm").stop().animate({'top' : '-400px'}, 200, function () {$("#noteForm").hide()});
+    $("#modal_opacity").fadeOut(200);
+    return false;
+  });
+
+
+
+
 });
